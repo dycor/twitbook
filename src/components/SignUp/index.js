4766438eaf05b63d03dd4147ea525651204bae32
elgrim <alexandre.vagnair@hetic.net>
2019-06-24 20:25:43 +0200

    [DEV] add sign in form

---
+++ src/components/SignUp/index.js
@@ -0,0 +1,56 @@
+import react from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import Router from './router.js'
+
+export default class SignUp extends react.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            errors: [],
+            auth: firebase.auth()
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        if (!this.state.email || !this.state.password) {
+            this.state.errors.push('Email ou mot de passe invalide');
+            return;
+        }
+
+        this.state.auth.createUserWithEmailAndPassword(this.state.email, this.state.password);
+        return
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                {this.state.errors.map(error => (
+                    <p key={error}>{error}</p>
+                ))}
+                <label>
+                    Email:
+                    <input type="email" value={this.state.email} onChange={this.handleChange} />
+                </label>
+
+                <label>
+                    Password:
+                    <input type="email" value={this.state.password} onChange={this.handleChange} />
+                </label>
+
+                <input type="submit" value="S'inscrire" />
+            </form>
+        )
+    }
+}
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from "react-router-dom";
 import Tweets from "./components/Tweets";
+import SignUp from "./components/SignUp"
 
 
 const Router = () => (
@@ -8,7 +9,7 @@ const Router = () => (
       {/*<PrivateRoute path="/logout" exact component={Logout} />*/}
       <Route path="/" exact component={Tweets} />
       {/*<Route path="/login" exact component={Login} />*/}
-      {/*<Route path="/signup" exact component={SignUp} />*/}
+      <Route path="/signup" exact component={SignUp} />
     </>
   )
 ;
