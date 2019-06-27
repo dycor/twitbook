2b88d2eb296b8ad6e2e996d3c8f54ee3130fac45
elgrim <alexandre.vagnair@hetic.net>
2019-06-27 14:19:24 +0200

    [DEV] add login

---
+++ src/components/SignIn/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import { Config } from "../../helpers/Config";
+
+export default class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.config = new Config();
+
+        this.state = {
+            email: '',
+            password: '',
+            errors: []
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [ event.target.name]: event.target.value,
+        });
+    }
+
+    handleSubmit(event) {
+        if (!this.state.email || !this.state.password) {
+            this.state.errors.push('Email ou mot de passe invalide');
+            return;
+        }
+
+        if (!firebase.apps.length) {
+            firebase.initializeApp(this.config.getEnv());
+        }
+
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then(user => {
+                console.info(user);
+                localStorage.setItem('logged', true)
+            }).catch(error => console.error(error))
+        ;
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                {this.state.errors.map(error => (
+                    <p key={error}>{error}</p>
+                ))}
+                <div className="form-group">
+                    <label>
+                        <span>Email:</span>
+                        <input type="email" value={this.state.email} onChange={this.handleChange} name="email" />
+                    </label>
+
+                    <label>
+                        <span>Password:</span>
+                        <input type="password" value={this.state.password} onChange={this.handleChange} name="password" />
+                    </label>
+                </div>
+
+                <input type="submit" value="S'inscrire" className="submit" />
+            </form>
+        )
+    }
+}
--- src/router.js
+++ src/router.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Route } from "react-router-dom";
 import Tweets from "./components/Tweets";
-import SignUp from "./components/SignUp"
+import SignUp from "./components/SignUp";
+import SignIn from './components/SignIn';
 
 
 const Router = () => (
     <>
       {/*<PrivateRoute path="/logout" exact component={Logout} />*/}
       <Route path="/" exact component={Tweets} />
-      {/*<Route path="/login" exact component={Login} />*/}
+      {<Route path="/login" exact component={SignIn} />}
       <Route path="/signup" exact component={SignUp} />
     </>
   )
