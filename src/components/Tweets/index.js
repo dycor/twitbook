fa07b0d0cf8e9ce09626f8b87189cbbfbe7a08b3
Correia Lopes Dylan <dylan.correia@hotmail.com>
2019-07-09 13:59:38 +0200

    add component tweet

--- src/components/Tweets/index.js
+++ src/components/Tweets/index.js
@@ -1,9 +1,11 @@
 import React,{ useState, useContext,useEffect,useRef,useMemo }  from 'react';
 import './style.scss';
 import {AppContext} from "../App/AppProvider";
+import Tweet from '../Tweet';
 
 var lastTweet;
 var indexDB ;
+
 const Tweets = () => {
 
   const [tweets,setTweets] = useState([]);
@@ -76,21 +78,8 @@ const Tweets = () => {
       </div>
     )}
       <ul className="tweetsList">
-        { tweets.map( tweet =>
-          <li key={tweet.id} id={tweet.id} onClick={ e => console.log(e.target.id)}>
-            <img src={tweet.profilImage} className="profilImage" />
-            <div>
-              <h3>{tweet.pseudo}</h3>
-              <span className="username">@{tweet.username}</span>
-            </div>
+        { tweets.map( tweet => <Tweet tweet={tweet} key={tweet.id}/>
 
-            <p>{tweet.text} </p>
-            <div>
-              <span><img className="icon" src="http://www.logospng.com/images/66/ajax-comment-system-for-laravel-66079.png"/>{tweet.nbComment}</span>
-              <span><img className="icon" src="https://previews.123rf.com/images/avectors/avectors1803/avectors180300188/98093154-heart-logo-vector-icon-isolated-modern-abstract-line-black-heart-symbol-.jpg"/>{tweet.nbLike}</span>
-              <span><img className="icon" src="https://www.nicepng.com/png/detail/24-241083_twitter-retweet-png.png"/>{tweet.nbRetweet}</span>
-            </div>
-          </li>
         )}
       </ul>
       { loading ? <div>Waiting .... </div> : <></>}
