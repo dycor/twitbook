0045d1247eca70bb4f9ef4b71605a0207fdfc501
dycor <dylan.correia@hotmail.com>
2019-07-17 17:05:23 +0200

    follow (#16)

--- src/router.js
+++ src/router.js
@@ -3,14 +3,17 @@ import { Route } from "react-router-dom";
 import Tweets from "./components/Tweets";
 import SignUp from "./components/SignUp";
 import SignIn from './components/SignIn';
+import Profile from './components/Profile';
+import PrivateRoute from './helpers/private-route';
 
 
 const Router = () => (
     <>
       {/*<PrivateRoute path="/logout" exact component={Logout} />*/}
-      <Route path="/" exact component={Tweets} />
-      {<Route path="/login" exact component={SignIn} />}
+      <PrivateRoute path="/" exact component={Tweets} />
+      <Route path="/login" exact component={SignIn} />
       <Route path="/signup" exact component={SignUp} />
+      <Route path="/profile/:username" exact component={Profile} />
     </>
   )
 ;
