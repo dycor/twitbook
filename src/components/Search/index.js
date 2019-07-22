f5a361f3edf70bad7b9dae0972c99f2c076ed5ca
dycor <dylan.correia@hotmail.com>
2019-07-22 20:00:31 +0200

    WIP : Search (#30)
    * Search wip
    
    * wip
    
    * css

---
+++ src/components/Search/index.js
@@ -0,0 +1,80 @@
+import React,{ useState, useContext } from 'react';
+import { AppContext } from "../App/AppProvider";
+import Tweet from "../Tweet";
+import Spinner from "../Spinner";
+import './style.scss'
+
+const Search = () => {
+  const [searchTerm,setSearchTerm] = useState('');
+  const [tweets,setTweets] = useState([]);
+  const [users,setUsers] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const [displayTweets,setDisplayTweets] = useState(true);
+  const { getStore } = useContext(AppContext);
+  const store = getStore();
+
+  const submitSearch = async () => {
+    setLoading(true);
+    await Promise.all([
+      store.collection('tweets')
+        .where('text', '==',searchTerm )
+        .get().then( allDocs => {
+        const tweetsRes = allDocs.docs.map(tweet => {
+          return {...tweet.data(), id: tweet.id};
+        });
+        setTweets(tweetsRes);
+        console.log(tweetsRes)
+        console.log(searchTerm)
+      }),
+      store.collection('users')
+        .where('username', '==',searchTerm )
+        .get().then( allDocs => {
+        const usersRes = allDocs.docs.map(user => {
+          return {...user.data(), id: user.id};
+        });
+        setUsers(usersRes);
+        console.log(usersRes);
+      })
+    ]);
+    setLoading(false);
+
+  };
+
+  return <div className="search-content">
+    <h1>Search</h1>
+    <div>
+      <input value={searchTerm} type="text" onChange={e => setSearchTerm(e.target.value)}/>
+      <button onClick={submitSearch}><span role="img" aria-label="search">🔍</span></button>
+    </div>
+    <div className="menu">
+      <span className={displayTweets ? 'active' : '' } onClick={() => setDisplayTweets(true)}> Tweets</span>
+      <span className={!displayTweets ? 'active' : '' } onClick={() => setDisplayTweets(false)}> Utilisateurs</span>
+    </div>
+    {
+      loading ?
+        <Spinner/>:
+        <>
+          {
+            displayTweets ?
+              <ul className="tweetsList">
+                {tweets.map(tweet => <Tweet key={Math.random()} tweet={tweet}/>)}
+              </ul>
+              :
+              <ul className="usersList">
+              {users.map(user =>
+                  <div>
+                    {JSON.stringify(user)}
+                  </div>)
+              }
+            </ul>
+          }
+
+        </>
+    }
+
+
+
+  </div> ;
+};
+
+export default Search;
--- src/router.js
+++ src/router.js
@@ -4,13 +4,14 @@ import Tweets from "./components/Tweets";
 import SignUp from "./components/SignUp";
 import SignIn from './components/SignIn';
 import Profile from './components/Profile';
+import Search from './components/Search';
 import PrivateRoute from './helpers/private-route';
 
 
 const Router = () => (
     <>
-      {/*<PrivateRoute path="/logout" exact component={Logout} />*/}
       <PrivateRoute path="/" exact component={Tweets} />
+      <Route path="/search" exact component={Search} />
       <Route path="/login" exact component={SignIn} />
       <Route path="/signup" exact component={SignUp} />
       <Route path="/profile/:username" exact component={Profile} />
